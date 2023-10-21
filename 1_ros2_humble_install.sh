93625f914220de0a4f2bf7d4879380183540dd98
t-shaped-person <kucira00@gmail.com>
2023-10-21 14:49:14 +0900

    20231021

---
+++ 1_ros2_humble_install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+echo "@@@@@ Set locale @@@@@"
+locale  # check for UTF-8
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+locale  # verify settings
+
+
+echo "@@@@@ Setup Sources @@@@@"
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+
+echo "@@@@@ Install ROS 2 packages @@@@@"
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y \
+	ros-humble-desktop \
+#	python3-argcomplete \
+	ros-dev-tools \
+	nano git net-tools openssh-server \
+#	python3-pip \
+#	python3-rosdep2 \
+#	ros-foxy-gazebo-ros \
+#	ros-foxy-navigation2 \
+#	ros-foxy-nav2-bringup \
+#	ros-foxy-nav2-map-server \
+#	ros-foxy-gazebo-ros-pkgs \
+#	ros-foxy-cartographer-ros \
+#	python3-colcon-common-extensions \
+
+
+echo "@@@@@ Sourcing the setup script @@@@@"
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+
+echo -e "\033[31m"ros2 humble installation is done"\033[0m"
+echo -e "\033[31m"system is rebooting in 5sec"\033[0m"
+sleep 1
+echo -e "\033[31m"system is rebooting in 4sec"\033[0m"
+sleep 1
+echo -e "\033[31m"system is rebooting in 3sec"\033[0m"
+sleep 1
+echo -e "\033[31m"system is rebooting in 2sec"\033[0m"
+sleep 1
+echo -e "\033[31m"system is rebooting in 1sec"\033[0m"
+sleep 1
+sudo reboot now
+exit 0
---
+++ 2_workspace_setup_r1d2.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo "@@@@@ install additional packages @@@@@"
+sudo apt install -y \
+	tilix \
+# pip3 install -U argcomplete pyserial
+
+
+echo "@@@@@ make workspace and colcon build @@@@@"
+mkdir -p ~/ros2_ws/src
+cd ~/ros2_ws
+colcon build
+
+
+echo "@@@@@ modify bashrc - shotcut, export @@@@@"
+echo "source ~/ros2_ws/install/setup.bash">>~/.bashrc
+echo "alias sai='sudo apt install'" >> ~/.bashrc
+echo "alias cw='cd ~/ros2_ws'" >> ~/.bashrc
+echo "alias cs='cd ~/ros2_ws/src'" >> ~/.bashrc
+echo "alias cb='cd ~/ros2_ws && colcon build --symlink-install'" >> ~/.bashrc
+echo "alias eb='nano ~/.bashrc'" >> ~/.bashrc
+echo "alias sb='source ~/.bashrc'" >> ~/.bashrc
+echo "#export ROS_DOMAIN_ID=77 # 0~101" >> ~/.bashrc
+echo "#ssh nuc@192.168.1.1" >> ~/.bashrc
+
+# rosdep install -i --from-path src --rosdistro foxy -y
+
+echo "@@@@@ ros2 packages clone and colcon build @@@@@"
+cd ~/ros2_ws/src
+git clone https://github.com/omorobot/omo_r1-foxy.git
+git clone https://github.com/YDLIDAR/YDLidar-SDK.git
+mkdir ~/ros2_ws/src/YDLidar-SDK/build
+cd ~/ros2_ws/src/YDLidar-SDK/build
+cmake ..
+make
+sudo make install
+git clone https://github.com/YDLIDAR/ydlidar_ros2_driver.git
+cd ~/ros2_ws
+colcon build
+colcon build
+
+
+echo -e "\033[31m"workspace setup is done"\033[0m"
+echo -e "\033[31m"system is rebooting in 5sec"\033[0m"
+sleep 1
+echo -e "\033[31m"system is rebooting in 4sec"\033[0m"
+sleep 1
+echo -e "\033[31m"system is rebooting in 3sec"\033[0m"
+sleep 1
+echo -e "\033[31m"system is rebooting in 2sec"\033[0m"
+sleep 1
+echo -e "\033[31m"system is rebooting in 1sec"\033[0m"
+sleep 1
+sudo reboot now
+exit 0
