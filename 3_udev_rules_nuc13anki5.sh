0134a36aa0727a59ad0a93ffbf4d23bd4e1c3ded
t-shaped-person <kucira00@gmail.com>
2024-09-07 15:43:17 +0900

    20240907

---
+++ 3_udev_rules_nuc13anki5.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "Motor Driver (USB Serial from RS232) : /dev/ttyUSBx to /dev/ttyMCU or /dev/ttyMotor :"
+if [ -f "/etc/udev/rules.d/98-omo-r1-mcu.rules" ]; then
+    echo "98-omo-r1-mcu.rules file already exist."
+else 
+    echo 'SUBSYSTEM=="tty", KERNELS=="3-7", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MODE:="0666", GROUP:="dialout", SYMLINK+="ttyMCU"' > /etc/udev/rules.d/98-omo-r1-mcu.rules
+    echo '98-omo-r1-mcu.rules created'
+fi
+if [ -f "/etc/udev/rules.d/98-omo-r1-mcu-1.rules" ]; then
+    echo "98-omo-r1-mcu-1.rules file already exist."
+else 
+    echo 'SUBSYSTEM=="tty", KERNELS=="3-7", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MODE:="0666", GROUP:="dialout", SYMLINK+="ttyMotor"' > /etc/udev/rules.d/98-omo-r1-mcu-1.rules
+    echo '98-omo-r1-mcu-1.rules created'
+fi
+
+
+echo "YD LiDAR (USB Serial) : /dev/ttyUSBx to /dev/ttyLIDAR or /dev/ttyLiDAR :"
+if [ -f "/etc/udev/rules.d/97-omo-r1-lidar.rules" ]; then
+    echo "97-omo-r1-lidar.rules file already exist."
+else 
+    echo 'SUBSYSTEM=="tty", KERNELS=="3-2", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MODE:="0666", GROUP:="dialout",  SYMLINK+="ttyLIDAR"' > /etc/udev/rules.d/97-omo-r1-lidar.rules
+    echo '97-omo-r1-lidar.rules created'
+fi
+if [ -f "/etc/udev/rules.d/97-omo-r1-lidar-1.rules" ]; then
+    echo "97-omo-r1-lidar-1.rules file already exist."
+else 
+    echo 'SUBSYSTEM=="tty", KERNELS=="3-2", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MODE:="0666", GROUP:="dialout",  SYMLINK+="ttyLiDAR"' > /etc/udev/rules.d/97-omo-r1-lidar-1.rules
+    echo '97-omo-r1-lidar-1.rules created'
+fi
+
+
+echo "@@@@@ reload rules @@@@@"
+udevadm control --reload-rules
+udevadm trigger
+exit 0
