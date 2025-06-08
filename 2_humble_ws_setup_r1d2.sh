6d8da681d01eaa1657ea84d0f217d66a4737b24b
t-shaped-person <t.shaped.person@gmail.com>
2025-06-08 17:04:15 +0900

    20250608

--- 2_humble_ws_setup_r1d2.sh
+++ 2_humble_ws_setup_r1d2.sh
@@ -29,10 +29,10 @@ echo "@@@@@ ros2 packages clone and colcon build @@@@@"
 cd ~/ros2_ws/src
 git clone https://github.com/omorobot/omo_r1-ros2.git -b dev_humble
 git clone https://github.com/YDLIDAR/ydlidar_ros2_driver.git -b humble
-git clone https://github.com/omorobot/YDLidar-SDK.git
-#git clone https://github.com/YDLIDAR/YDLidar-SDK.git
+#git clone https://github.com/omorobot/YDLidar-SDK.git
+git clone https://github.com/YDLIDAR/YDLidar-SDK.git
 cd ~/ros2_ws
-rosdep install -i --from-path src --rosdistro humble -y
+#rosdep install -i --from-path src --rosdistro humble -y
 mkdir ~/ros2_ws/src/YDLidar-SDK/build
 cd ~/ros2_ws/src/YDLidar-SDK/build
 cmake ..
