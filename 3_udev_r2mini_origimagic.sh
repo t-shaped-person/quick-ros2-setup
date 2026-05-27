0b09f5e4674b0c82f73b44680eca58eab7e02d89
t-shaped-person <t.shaped.person@gmail.com>
2026-05-27 20:24:40 +0900

    20260527

--- 3_udev_r2mini_origimagic.sh
+++ 3_udev_r2mini_origimagic.sh
@@ -4,7 +4,7 @@ echo "YD LiDAR (USB Serial) : /dev/ttyUSBx to /dev/ttyLIDAR :"
 if [ -f "/etc/udev/rules.d/97-r2mini-lidar.rules" ]; then
     echo '97-r2mini-lidar.rules file already exist.'
 else
-    echo 'SUBSYSTEM=="tty", KERNELS=="1-2", MODE:="0666", GROUP:="dialout", SYMLINK+="ttyLIDAR"' > /etc/udev/rules.d/97-r2mini-lidar.rules
+    echo 'SUBSYSTEMS=="usb", KERNELS=="1-2", MODE:="0666", GROUP:="dialout", SYMLINK+="ttyLIDAR"' > /etc/udev/rules.d/97-r2mini-lidar.rules
     echo '97-r2mini-lidar.rules created'
 fi
 
@@ -13,7 +13,7 @@ echo "Motor Driver (USB Serial from RS232) : /dev/ttyUSBx to /dev/ttyMCU:"
 if [ -f "/etc/udev/rules.d/98-r2mini-mcu.rules" ]; then
     echo '98-r2mini-mcu.rules file already exist.'
 else
-    echo 'SUBSYSTEM=="tty", KERNELS=="1-3", MODE:="0666", GROUP:="dialout", SYMLINK+="ttyMCU"' > /etc/udev/rules.d/98-r2mini-mcu.rules
+    echo 'SUBSYSTEMS=="usb", KERNELS=="1-3", MODE:="0666", GROUP:="dialout", SYMLINK+="ttyMCU"' > /etc/udev/rules.d/98-r2mini-mcu.rules
     echo '98-r2mini-mcu.rules created'
 fi
 
